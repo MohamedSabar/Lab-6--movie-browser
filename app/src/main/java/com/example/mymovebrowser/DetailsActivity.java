d09aa4c81e247b760f2125070d3d26a011bb5f21
mohamedsabbar <mohamed.mo.sabar@gmail.com>
2021-11-14 13:45:43 +0300

    lab6 movie-browser

---
+++ app/src/main/java/com/example/mymovebrowser/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.mymovebrowser;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Movie movie  = getIntent().getParcelableExtra("movie");
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        fts.add(R.id.container,DetailsFragment.newInstance(movie));
+    fts.commit();}
+}
